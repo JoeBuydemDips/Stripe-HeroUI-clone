d48d782f1f1403804d75101528ef4f5d1f604261
JoeBuydemDips <ksarkodie111@icloud.com>
2022-03-03 18:45:52 -0500

    Build Hero.

--- src/Hero.js
+++ src/Hero.js
@@ -3,8 +3,25 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of companies of all sizes from startups to Fortune 500s use
+            Stripe's software and APIs to accept payments, send payouts, and
+            manage their business online.
+          </p>
+          <button className="btn">start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
