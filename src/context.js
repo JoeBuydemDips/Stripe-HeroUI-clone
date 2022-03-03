42f312347656e7fc2308af6081a8e34aad3f34d4
JoeBuydemDips <ksarkodie111@icloud.com>
2022-03-03 18:23:34 -0500

    Build context. Import Appprovider in App.js

--- src/context.js
+++ src/context.js
@@ -1,2 +1,45 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOPen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOPen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOPen,
+        isSubmenuOPen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+//custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
